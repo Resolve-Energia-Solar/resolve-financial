826046ff84b23621f7cb77c23de14cc74c1b0911
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-03-11 11:15:49 -0300

    fix: fix success_url on TaskCreateView

--- main/views.py
+++ main/views.py
@@ -15,10 +15,11 @@ class TasksView(ListView):
     model = Task
     template_name = "tasks.html"
     context_object_name = "tasks"
+    paginate_by = 10
 
 
 class TaskCreateView(CreateView):
     model = Task
     fields = "__all__"
     template_name = "task_create.html"
-    success_url = reverse_lazy("tasks")
+    success_url = reverse_lazy("main:tasks")
