d1f5dfe48b8ac9d07b9449151e74c4085487ba65
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-03-11 11:14:56 -0300

    feat: create user list and user creation pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("usuarios/", UsersListView.as_view(), name="users_list"),
+    path("usuarios/criar/", UserCreateView.as_view(), name="user_create"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,17 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from django.contrib.auth import get_user_model
 
 
 class UsersListView(ListView):
     model = get_user_model()
     template_name = "accounts/users.html"
+    context_object_name = "users"
+    paginate_by = 10
 
+
+class UserCreateView(CreateView):
+    model = get_user_model()
+    fields = "__all__"
+    template_name = "accounts/user_create.html"
+    success_url = reverse_lazy("accounts:users")
