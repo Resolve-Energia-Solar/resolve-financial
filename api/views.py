dca56f757ffbe0d39024b201bd07c4dd7264e894
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-09-17 11:37:07 -0300

    fix: solve timezone.now() error

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from django.utils import timezone
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
