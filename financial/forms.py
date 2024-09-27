11e25aaac685ca9aca4a1991c91859cac64b8f9c
Leonardo Costa <leonardoabreu1212@gmail>
2024-09-27 20:09:18 -0300

    feat: add currency input

--- financial/forms.py
+++ financial/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from .models import PaymentRequest
 from django_select2.forms import Select2Widget, HeavySelect2Widget
+from django.utils.safestring import mark_safe
+
+
+class CurrencyInput(forms.TextInput):
+    def render(self, name, value, attrs=None, renderer=None):
+        widget_html = super().render(name, value, attrs, renderer)
+        return mark_safe(f'<div class="input-group mb-3">'
+                         f'<span class="input-group-text">R$</span>'
+                         f'{widget_html}'
+                         f'</div>')
 
 
 class PaymentRequestForm(forms.ModelForm):
     
     supplier_name = forms.CharField(widget=forms.HiddenInput(), required=False)
     supplier_cpf = forms.CharField(widget=forms.HiddenInput(), required=False)
+
     
     class Meta:
         model = PaymentRequest
@@ -20,10 +31,11 @@ class PaymentRequestForm(forms.ModelForm):
             'requester': Select2Widget(attrs={'data-placeholder': 'Selecionar solicitante'}),
             'manager': Select2Widget(attrs={'data-placeholder': 'Selecionar aprovador'}),
             'supplier': HeavySelect2Widget(data_view='financial:suppliers_list', attrs={'data-placeholder': 'Selecionar fornecedor', 'data-minimum-input-length': 11, 'data-ajax--delay': 1000}),
-            'category': HeavySelect2Widget(data_view='financial:categories_list', attrs={'data-placeholder': 'Selecionar categoria', 'data-minimum-input-length': 3, 'data-ajax--delay': 1000}),
+            'category': HeavySelect2Widget(data_view='financial:categories_list', attrs={'data-placeholder': 'Selecionar categoria', 'data-minimum-input-length': 3, 'data-ajax--delay': 1000}),    
             'sale': Select2Widget(attrs={'data-placeholder': 'Selecionar venda', 'data-minimum-input-length': 3}),
             'service_date': forms.DateInput(attrs={'type': 'date'}),
             'due_date': forms.DateInput(attrs={'type': 'date'}),
             'payment_method': Select2Widget(attrs={'data-placeholder': 'Selecionar forma de pagamento'}),
             'causative_department': Select2Widget(attrs={'data-placeholder': 'Selecionar departamento'}),
+            'amount': CurrencyInput(attrs={'class': 'mask-money'}),
         }
