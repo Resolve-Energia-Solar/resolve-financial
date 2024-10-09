68ce841243b7ebf802c2855e9c179dbdb6d01498
Leonardo Costa <leonardoabreu1212@gmail>
2024-10-09 13:31:35 -0300

    feat: add ContentType and permission to admin page

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
 from django.contrib import admin
+
 from .models import Board, Column
 from .models import Task
 
@@ -18,3 +21,22 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ("title", "description", "owner", "board", "is_completed", "start_date", "due_date", "is_completed_date")
     list_filter = ("is_completed",)
     search_fields = ("title", "description", "owner", "board", "is_completed", "start_date", "due_date", "is_completed_date")
+
+
+@admin.register(ContentType)
+class ContentTypeAdmin(admin.ModelAdmin):
+    list_display = ("app_label", "model")
+    list_display_links = ("app_label", "model")
+    search_fields = ("app_label", "model")
+    list_per_page = 10
+    list_max_show_all = 100
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ("name", "content_type", "codename")
+    list_display_links = ("name", "content_type", "codename")
+    search_fields = ("name", "content_type", "codename")
+    list_filter = ("content_type",)
+    list_per_page = 10
+    list_max_show_all = 100
