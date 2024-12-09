713604225e0f2f4012788d8645f1ee8124f60953
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-12-09 08:48:21 -0300

    refactor: change phone_number max length

--- accounts/models.py
+++ accounts/models.py
@@ -92,10 +92,9 @@ class User(AbstractUser):
 class PhoneNumber(models.Model):
     country_code = models.PositiveSmallIntegerField("Código do País", default=55)
     area_code = models.PositiveSmallIntegerField("DDD", validators=[RegexValidator(r'^\d{2}$'), MaxValueValidator(99)])
-    phone_number = models.CharField("Número de Telefone", max_length=20, validators=[RegexValidator(r'^\d+$')])
+    phone_number = models.CharField("Número de Telefone", max_length=11, validators=[RegexValidator(r'^\d+$')])
     is_main = models.BooleanField("Principal?", default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Usuário", related_name="phone_numbers", blank=True, null=True)
-    
     def __str__(self):
         return f'+{self.country_code} {self.phone_number}'
     
