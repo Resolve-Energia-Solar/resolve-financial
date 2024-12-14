d9536a0c82e0f97f9c286eecec69886cbb375c71
Thiago Menezes <thimenezes19@gmail.com>
2024-12-14 09:15:56 -0300

    feat: remove to represatation

--- core/serializers.py
+++ core/serializers.py
@@ -24,11 +24,6 @@ class DocumentTypeSerializer(BaseSerializer):
         fields = '__all__'
         read_only_fields = ['id', 'created_at', 'updated_at']
         
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data['app_label'] = instance.get_app_label_display()
-        return data
-
 
 class AttachmentSerializer(BaseSerializer):
     
