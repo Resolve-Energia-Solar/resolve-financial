0677a102fcd40264a396951bef08e937cfe9d0c9
Thiago Menezes <thimenezes19@gmail.com>
2024-12-26 16:58:17 -0300

    Revert "fix: fix seralizer"
    This reverts commit f10108038f8567666bf084b7fb87b63588424ba0.

--- api/serializers.py
+++ api/serializers.py
@@ -1,36 +1,43 @@
 from rest_flex_fields import FlexFieldsModelSerializer
-from django.contrib.contenttypes.models import ContentType
+from django.apps import apps
 
 class BaseSerializer(FlexFieldsModelSerializer):
+    
     class Meta:
         model = None
         exclude = []
-
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Remove o campo 'is_deleted' se existir
         if 'is_deleted' in self.fields:
             self.fields.pop('is_deleted')
-
+                
     def get_expandable_fields(self):
+        """
+        Retorna campos expansíveis dinamicamente com base nos relacionamentos do modelo.
+        """
         expandable_fields = {}
         related_fields = [
-            f for f in self.Meta.model._meta.get_fields()
-            if f.is_relation and f.related_model
+            field for field in self.Meta.model._meta.get_fields()
+            if field.is_relation and field.related_model is not None
         ]
         for field in related_fields:
             related_model = field.related_model
-            if related_model == ContentType:
-                # Use o seu ContentTypeSerializer específico
-                serializer_path = 'accounts.serializers.ContentTypeSerializer'
-            else:
-                # Gera o path normalmente
-                serializer_name = f"{related_model.__name__}Serializer"
-                serializer_path = f"{related_model._meta.app_label}.{serializer_name}"
+            serializer_name = f"{related_model.__name__}Serializer"
+            serializer_path = f"{related_model._meta.app_label}.{serializer_name}"
 
+            # Determina se o relacionamento é muitos (1-M ou M-M)
             is_many = field.one_to_many or field.many_to_many
-            expandable_fields[field.name] = (serializer_path, {'many': is_many})
-        return expandable_fields
 
+            # Adiciona ao dicionário de campos expansíveis
+            expandable_fields[field.name] = (serializer_path, {'many': is_many})
+        
+        return expandable_fields
+    
     @property
     def expandable_fields(self):
+        """
+        Retorna os expandable_fields dinamicamente.
+        """
         return self.get_expandable_fields()
