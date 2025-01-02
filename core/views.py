ec2a2bfc7a82d3a085110f882aad05ae190e921e
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-01-02 17:21:54 -0300

    feat: HistoryView can display related objects histories too

--- core/views.py
+++ core/views.py
@@ -5,10 +5,11 @@ from django.utils.text import capfirst
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.db.models import ForeignKey, OneToOneField
+from django.apps import apps
 
 from api.views import BaseModelViewSet
 from notifications.models import Notification
-
 from resolve_crm.models import Sale
 
 from .models import *
@@ -70,6 +71,7 @@ class HistoryView(APIView):
     def get(self, request):
         content_type_id = request.query_params.get('content_type')
         object_id = request.query_params.get('object_id')
+        get_related = request.query_params.get('get_related', 'false').lower() == 'true'
 
         if not content_type_id or not object_id:
             return Response({
@@ -79,68 +81,78 @@ class HistoryView(APIView):
         # Busca o ContentType ou retorna 404 se não encontrado
         content_type = get_object_or_404(ContentType, id=content_type_id)
         model_class = content_type.model_class()
-        
-        # Verifica se o model possui histórico
-        if not hasattr(model_class, 'history'):
-            return Response({
-                'message': 'O modelo não possui histórico.'
-            }, status=status.HTTP_400_BAD_REQUEST)
 
+        # Busca o objeto principal
         try:
-            # Busca o histórico para o objeto específico
-            history = model_class.history.filter(id=object_id)
-            if not history.exists():
-                return Response({
-                    'message': 'Histórico não encontrado para o objeto fornecido.'
-                }, status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
+            obj = model_class.objects.get(id=object_id)
+        except model_class.DoesNotExist:
             return Response({
-                'message': 'Erro ao buscar o histórico.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        except Exception as e:
-            return Response({
-                'message': 'Erro ao buscar o histórico.',
-                'error': str(e)
-            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                'message': 'Objeto não encontrado.'
+            }, status=status.HTTP_404_NOT_FOUND)
 
-        # Calcula as diferenças entre todas as versões consecutivas
-        changes = []
-        history_objects = list(history)
+        # Função para buscar histórico de um objeto
+        def get_object_history(object_instance, related_model_name=None, related_object_id=None):
+            if not hasattr(object_instance, 'history'):
+                return []
 
-        for i in range(len(history_objects) - 1):
-            new_record = history_objects[i]
-            old_record = history_objects[i + 1]
-            delta = new_record.diff_against(old_record)
+            history = object_instance.history.all()
+            changes = []
+            history_objects = list(history)
 
-            if delta.changes:
-                author_data = RelatedUserSerializer(new_record.history_user).data if new_record.history_user else {'username': 'Desconhecido'}
-                change_list = []
-                for change in delta.changes:
-                    field_name = change.field
-                    # Obter o label do campo
-                    try:
-                        field_verbose = new_record._meta.get_field(field_name).verbose_name
-                    except FieldDoesNotExist:
-                        field_verbose = field_name  # Usa o nome do campo se o verbose_name não estiver definido
-                    field_verbose = capfirst(field_verbose)
-                    change_list.append({
-                        'field': field_name,
-                        'field_label': field_verbose,
-                        'old': change.old,
-                        'new': change.new
+            for i in range(len(history_objects) - 1):
+                new_record = history_objects[i]
+                old_record = history_objects[i + 1]
+                delta = new_record.diff_against(old_record)
+
+                if delta.changes:
+                    author_data = RelatedUserSerializer(new_record.history_user).data if new_record.history_user else {'username': 'Desconhecido'}
+                    change_list = []
+                    for change in delta.changes:
+                        field_name = change.field
+                        try:
+                            field_verbose = new_record._meta.get_field(field_name).verbose_name
+                        except FieldDoesNotExist:
+                            field_verbose = field_name
+                        field_verbose = capfirst(field_verbose)
+                        change_list.append({
+                            'field': field_name,
+                            'field_label': field_verbose,
+                            'old': change.old,
+                            'new': change.new
+                        })
+                    changes.append({
+                        'timestamp': new_record.history_date,
+                        'history_type': new_record.history_type,
+                        'author': author_data,
+                        'model': related_model_name or object_instance.__class__.__name__,
+                        'object_id': related_object_id or object_instance.id,
+                        'changes': change_list
                     })
-                changes.append({
-                    'version_diff': f"{i + 1} -> {i + 2}",
-                    'author': author_data,
-                    'timestamp': new_record.history_date,
-                    'history_type': new_record.history_type,
-                    'changes': change_list
-                })
 
-        # Retornar somente as mudanças
+            return changes
+
+        # Consolidar histórico
+        unified_history = []
+
+        # Adiciona histórico do objeto principal
+        unified_history.extend(get_object_history(obj))
+
+        # Busca objetos relacionados dinamicamente apenas se `get_related` for `true`
+        if get_related:
+            for model in apps.get_models():
+                for field in model._meta.fields:
+                    if isinstance(field, (ForeignKey, OneToOneField)) and field.related_model == model_class:
+                        related_objects = model.objects.filter(**{field.name: obj})
+                        for related_obj in related_objects:
+                            unified_history.extend(
+                                get_object_history(related_obj, related_model_name=model.__name__, related_object_id=related_obj.id)
+                            )
+
+        # Ordena o histórico pela timestamp
+        unified_history = sorted(unified_history, key=lambda x: x['timestamp'], reverse=True)
+
         return Response({
-            'changes': changes
+            'history': unified_history
         }, status=status.HTTP_200_OK)
 
 
