c79489be589f46f1c00841e300ea1597f7b1270a
Thiago Menezes <thimenezes19@gmail.com>
2025-01-24 15:53:45 -0300

    feat: add new columns from sales admin

--- resolve_crm/admin.py
+++ resolve_crm/admin.py
@@ -29,7 +29,7 @@ class ComercialProposalAdmin(admin.ModelAdmin):
 
 @admin.register(Sale)
 class SaleAdmin(admin.ModelAdmin):
-    list_display = ("total_value", "contract_number", "can_generate_contract", "total_paid", "created_at")
+    list_display = ("customer" ,"contract_number", "total_value", "total_paid","signature_date", "created_at")
     inlines = [SaleProductInline, PaymentInline]
     search_fields = ("contract_number", "customer__username", "seller__username")
     list_filter = ("payment_status", "status", "created_at")
