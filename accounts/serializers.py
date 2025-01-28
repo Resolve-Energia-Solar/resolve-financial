ccc0ae7c4142323cb27f1bd974713080003d5109
Thiago Menezes <thimenezes19@gmail.com>
2025-01-28 14:11:28 -0300

    feat: add validation from user first document

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -177,7 +177,8 @@ class UserSerializer(BaseSerializer):
         
     def validate_first_document(self, value):
         value = value.replace('.', '').replace('-', '').replace('/', '')
-        
+
+        # Verifica se é um CPF ou CNPJ válido
         if len(value) == 11:  # CPF
             if not CPF().validate(value):
                 raise serializers.ValidationError("CPF inválido.")
@@ -186,6 +187,19 @@ class UserSerializer(BaseSerializer):
                 raise serializers.ValidationError("CNPJ inválido.")
         else:
             raise serializers.ValidationError("Número inválido. Insira um CPF ou CNPJ válido.")
+
+        # Verifica se está criando um novo usuário ou alterando o CPF/CNPJ
+        if self.instance:
+            # Se for edição, verifica se o CPF/CNPJ foi alterado
+            if self.instance.first_document != value:
+                user_exists = User.objects.filter(first_document=value).exists()
+                if user_exists:
+                    raise serializers.ValidationError("CPF/CNPJ já cadastrado.")
+        else:
+            # Caso seja criação de um novo usuário
+            user_exists = User.objects.filter(first_document=value).exists()
+            if user_exists:
+                raise serializers.ValidationError("CPF/CNPJ já cadastrado.")
 
         return value
         
