c8e80d905887c3e9c78a8430905638df664e7f2c
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-01-30 17:33:36 -0300

    feat: create post_save for FinancialRecord

--- financial/signals.py
+++ financial/signals.py
@@ -1,8 +1,17 @@
+import logging
+import os
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
+from financial.models import FinancialRecord
+from financial.views import OmieIntegrationView
 from resolve_crm.models import Sale
 from decimal import Decimal
 from django.core.exceptions import ValidationError
+import requests
+
+
+logger = logging.getLogger(__name__)
+
 
 @receiver(pre_save, sender=Sale)
 def store_old_total_value(sender, instance, **kwargs):
@@ -55,3 +64,48 @@ def adjust_franchise_installments_on_sale_update(sender, instance, created, **kw
                     installment.installment_value = installment_value
                     installment.full_clean()
                     installment.save()
+
+
+@receiver(post_save, sender=FinancialRecord)
+def request_responsible_approval(sender, instance, created, **kwargs):
+    """
+    Solicita a aprovação do responsável pelo usuário que criou o registro.
+    """
+    if created:
+        if instance.category_code in ['2.02.94', '2.02.92']:
+            instance.status = 'E'
+            instance.save()
+            logger.info(f"Registro Financeiro {instance.protocol} aprovado automaticamente.")
+            try:
+                OmieIntegrationView().create_payment_request(instance)
+            except Exception as e:
+                logger.error(f"Erro ao criar solicitação de pagamento no Omie: {e}")
+                raise ValidationError(f"Erro ao criar solicitação de pagamento no Omie: {e}")
+        else:
+            FINANCIAL_RECORD_APPROVAL_URL = os.environ.get('FINANCIAL_RECORD_APPROVAL_URL', None)
+            if not FINANCIAL_RECORD_APPROVAL_URL:
+                raise Exception("URL de aprovação não configurada.")
+            
+            if instance.responsible:
+                try:
+                    # Enviar notificação para o responsável
+                    url = FINANCIAL_RECORD_APPROVAL_URL
+                    body = {
+                        'id': instance.id,
+                        'manager_email': instance.responsible.email,
+                        'description': (
+                            f'Solicitação de Pagamento nº {instance.protocol}\n'
+                            f'Criada em: {instance.created_at.strftime("%d/%m/%Y %H:%M:%S")}\n'
+                            f'Requisitante: {instance.requester.complete_name}\n'
+                            f'Setor: {instance.requesting_department.name}\n'
+                            f'Valor: R$ {instance.value:.2f}\n'
+                            f'Descrição: {instance.notes}'
+                        ),
+                    }
+                    response = requests.post(url, json=body)
+                    response.raise_for_status()
+                except requests.RequestException as e:
+                    logger.error(f"Erro ao solicitar aprovação: {e}")
+                    raise ValidationError(f"Erro ao solicitar aprovação: {e}")
+            else:
+                raise ValidationError("Responsável não configurado para a solicitação de pagamento.")
