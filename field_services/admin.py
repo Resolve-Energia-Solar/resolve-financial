c168dc63f0e57a2c3c2c275d8c4db56914ceb572
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-02-19 11:01:16 -0300

    feat: add display_leads() to ScheduleAdmin

--- field_services/admin.py
+++ field_services/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from field_services.models import *
 
@@ -29,6 +31,33 @@ class DeadlineAdmin(admin.ModelAdmin):
 @admin.register(Schedule)
 class ScheduleAdmin(admin.ModelAdmin):
     list_display = ("schedule_date", "project", "service")
+    readonly_fields = ("display_leads",)
+
+    def display_leads(self, obj):
+        leads = obj.lead_inspections.all()
+        if not leads:
+            return "Sem leads"
+        html = '<table class="table table-bordered">'
+        html += """
+            <thead class="thead-light">
+                <tr>
+                    <th scope="col">Lead</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Telefone</th>
+                </tr>
+            </thead>
+            <tbody>
+        """
+        for lead in leads:
+            url = reverse(
+                f"admin:{lead._meta.app_label}_{lead._meta.model_name}_change",
+                args=[lead.pk]
+            )
+            email = lead.contact_email if lead.contact_email else "-"
+            html += f"<tr><td><a href='{url}'>{lead}</a></td><td>{email}</td><td>{lead.phone}</td></tr>"
+        html += "</tbody></table>"
+        return mark_safe(html)
+    display_leads.short_description = "Leads"
 
 @admin.register(BlockTimeAgent)
 class BlockTimeAgentAdmin(admin.ModelAdmin):
