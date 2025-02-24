7f30bdd87e83598b85a2e3b0e8f6e992cb2eefc3
Thiago Menezes <thimenezes19@gmail.com>
2025-02-24 09:37:02 -0300

    feat: change CustomLimitOffsetPagination

--- api/pagination.py
+++ api/pagination.py
@@ -1,10 +1,12 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 
 class CustomLimitOffsetPagination(PageNumberPagination):
-    page_size = 15  # Limite padrão de itens por página
-    page_size_query_param = 'limit'  # Nome do parâmetro para o limite
-    page_query_param = 'page'  # Nome do parâmetro para a página
-    max_page_size = 100  # Limite máximo de itens por página
+    page_size = 15 
+    page_size_query_param = 'limit' 
+    page_query_param = 'page' 
+    max_page_size = 100 
 
     def get_page_size(self, request):
         try:
@@ -14,3 +16,18 @@ class CustomLimitOffsetPagination(PageNumberPagination):
             return min(page_size, self.max_page_size)
         except (ValueError, TypeError):
             return self.page_size
+
+    def paginate_queryset(self, queryset, request, view=None):
+        try:
+            return super().paginate_queryset(queryset, request, view)
+        except NotFound:
+            self.page = None
+            return []
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count if self.page else 0,
+            'next': self.get_next_link() if self.page else None,
+            'previous': self.get_previous_link() if self.page else None,
+            'results': data
+        })
