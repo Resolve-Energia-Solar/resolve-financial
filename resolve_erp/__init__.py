73ab0014154268463382e54f993abfb39987ec74
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-02-26 12:03:04 -0300

    perf: add Celery and RabbitMQ

--- resolve_erp/__init__.py
+++ resolve_erp/__init__.py
@@ -1,5 +1,4 @@
-from __future__ import absolute_import, unicode_literals
-# Garante que o Celery é carregado com o Django
-from .celery import app as celery_app
 
-__all__ = ('celery_app',)
+from resolve_erp.celery import app
+
+__all__ = ('app',)
