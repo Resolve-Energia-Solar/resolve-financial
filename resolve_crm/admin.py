ebc4e853cf3b662bd9f98f0bf5bb36b0b5a81518
Thiago Menezes <thimenezes19@gmail.com>
2025-02-27 23:32:41 -0300

    fix: fix suplier to supplier from sale admin

--- resolve_crm/admin.py
+++ resolve_crm/admin.py
@@ -30,7 +30,7 @@ class ComercialProposalAdmin(admin.ModelAdmin):
 @admin.register(Sale)
 class SaleAdmin(admin.ModelAdmin):
     list_display = ("customer" ,"contract_number", "total_value", "total_paid","signature_date", "billing_date", "created_at")
-    supplier = ["customer", "seller", "sales_supervisor","sales_manager", "supplier"]
+    autocomplete_fields = ["customer", "seller", "sales_supervisor","sales_manager", "supplier"]
     inlines = [SaleProductInline, PaymentInline]
     search_fields = ("contract_number", "customer__username", "seller__username")
     list_filter = ("payment_status", "status", "created_at")
