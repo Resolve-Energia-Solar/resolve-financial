ff2d5c48663e520b0643729ffb4dc098df7b199e
Thiago Menezes <thimenezes19@gmail.com>
2025-03-28 11:25:45 -0300

    FIX: FIX PAGINATION

--- api/pagination.py
+++ api/pagination.py
@@ -1,5 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 
 class CustomPagination(PageNumberPagination):
     page_size = 10
@@ -10,6 +11,13 @@ class CustomPagination(PageNumberPagination):
     def __init__(self):
         super().__init__()
         self.extra_meta = {}
+        
+    def paginate_queryset(self, queryset, request, view=None):
+        try:
+            return super().paginate_queryset(queryset, request, view)
+        except NotFound:
+            self.page = self.dummy_empty_page()
+            return []
 
     def get_paginated_response(self, data):
         return Response({
