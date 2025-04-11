33aba7bd889cf53dc3ab0dd3471a16510e680ba3
Thiago Menezes <thimenezes19@gmail.com>
2025-04-11 15:11:33 -0300

    feat: add string method to serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,8 +2,10 @@ from rest_flex_fields import FlexFieldsModelSerializer
 from django.contrib.contenttypes.models import ContentType
 from django.utils.functional import cached_property
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 class BaseSerializer(FlexFieldsModelSerializer, ModelSerializer):
+    str = serializers.SerializerMethodField()
     class Meta:
         model = None
 
@@ -25,3 +27,18 @@ class BaseSerializer(FlexFieldsModelSerializer, ModelSerializer):
                 is_many = field.many_to_many or field.one_to_many
                 expandable[field.name] = (serializer_path, {'many': is_many})
         return expandable
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        request = self.context.get('request')
+        if request:
+            requested_fields = request.query_params.get('fields', '')
+            requested_fields = requested_fields.split(',')
+            if 'str' not in requested_fields:
+                self.fields.pop('str', None)
+        else:
+            self.fields.pop('str', None)
+
+    def get_str(self, obj):
+        return str(obj)
--- field_services/serializers.py
+++ field_services/serializers.py
@@ -1,6 +1,5 @@
 from field_services.models import *
 from accounts.serializers import BaseSerializer
-from rest_framework import serializers
 
 class RoofTypeSerializer(BaseSerializer):
     class Meta(BaseSerializer.Meta):
@@ -33,14 +32,25 @@ class FormsSerializer(BaseSerializer):
         
         
 class ScheduleSerializer(BaseSerializer):
-    str = serializers.SerializerMethodField()
+    # str = serializers.SerializerMethodField()
     
     class Meta(BaseSerializer.Meta):
         model = Schedule
         fields = '__all__'
         
-    def get_str(self, obj):
-        return str(obj)
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     requested_fields = self.context.get('request').query_params.get('fields')
+    #     if requested_fields:
+    #         requested_fields = requested_fields.split(',')
+    #         if 'str' not in requested_fields:
+    #             self.fields.pop('str', None)
+    #     else:
+    #         self.fields.pop('str', None)
+
+    # def get_str(self, obj):
+    #     return str(obj)
     
 
 class AnswerSerializer(BaseSerializer):
