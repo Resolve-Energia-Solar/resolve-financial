3f9228d2d8f8b1e610e4f1cb4e8170c159eacdf3
matheusmacias <matheusmacias3@gmail.com>
2025-04-20 17:48:06 -0300

    refactor: extract conflict-handling logic into separate methods

--- field_services/serializers.py
+++ field_services/serializers.py
@@ -37,19 +37,11 @@ class FormsSerializer(BaseSerializer):
         
         
 class ScheduleSerializer(BaseSerializer):
-    # str = serializers.SerializerMethodField()
-    
     class Meta(BaseSerializer.Meta):
         model = Schedule
         fields = '__all__'
-    
-    def create(self, validated_data):
-        schedule_agent = validated_data.get("schedule_agent")
-        schedule_date = validated_data.get("schedule_date")
-        schedule_start_time = validated_data.get("schedule_start_time")
-        schedule_end_time = validated_data.get("schedule_end_time")
-        address = validated_data.get("address")
 
+    def validate_agent_availability(self, schedule_agent, schedule_date, schedule_start_time, schedule_end_time):
         disponibility = FreeTimeAgent.objects.filter(
             agent=schedule_agent,
             day_of_week=schedule_date.weekday(),
@@ -70,12 +62,17 @@ class ScheduleSerializer(BaseSerializer):
                     "end": disponibility.end_time.strftime("%H:%M")
                 }]
             })
+        return disponibility
 
+    def check_schedule_conflicts(self, schedule_agent, schedule_date, schedule_start_time, schedule_end_time, instance=None, disponibility=None):
         existing_schedules = Schedule.objects.filter(
             schedule_agent=schedule_agent,
             schedule_date=schedule_date,
             is_deleted=False
         ).order_by('schedule_start_time')
+
+        if instance:
+            existing_schedules = existing_schedules.exclude(pk=instance.pk)
 
         conflicting_schedules = existing_schedules.filter(
             models.Q(
@@ -106,141 +103,163 @@ class ScheduleSerializer(BaseSerializer):
                 "message": "Já existe um agendamento para este agente no horário selecionado.",
                 "available_time": available_slots
             })
+        return existing_schedules
 
-        # Check travel time feasibility
+    def calculate_travel_times(self, address, all_schedules, position):
+        travel_time_previous = 0
+        travel_time_next = 0
+        total_travel_time = 0
+        
         try:
-            available_slots = []
-            all_schedules = list(existing_schedules)
-            has_conflict = False
-            travel_time_messages = []
-            best_position = None
-            min_travel_time = float('inf')
-
-            # Find the best position to insert the new schedule
-            for i in range(len(all_schedules) + 1):
-                travel_time_previous = 0
-                travel_time_next = 0
-                total_travel_time = 0
-                
-                # Calculate travel time from previous schedule
-                if i > 0:
-                    origin = f"{all_schedules[i-1].address.latitude},{all_schedules[i-1].address.longitude}"
-                    destination = f"{address.latitude},{address.longitude}"
-                    route = gmaps.directions(origin, destination, mode="driving")
-                    travel_time_previous = int(route[0]['legs'][0]['duration']['value'] / 60)
-                    total_travel_time += travel_time_previous
-                
-                # Calculate travel time to next schedule
-                if i < len(all_schedules):
-                    origin = f"{address.latitude},{address.longitude}"
-                    destination = f"{all_schedules[i].address.latitude},{all_schedules[i].address.longitude}"
-                    route = gmaps.directions(origin, destination, mode="driving")
-                    travel_time_next = int(route[0]['legs'][0]['duration']['value'] / 60)
-                    total_travel_time += travel_time_next
-
-                # Check if there's enough time for this position
-                if i == 0:  # Before first schedule
-                    if all_schedules:
-                        first_schedule = all_schedules[0]
-                        if first_schedule.schedule_start_time > disponibility.start_time:
-                            latest_end = first_schedule.schedule_start_time.replace(
-                                hour=(first_schedule.schedule_start_time.hour * 60 + first_schedule.schedule_start_time.minute - travel_time_next) // 60,
-                                minute=(first_schedule.schedule_start_time.hour * 60 + first_schedule.schedule_start_time.minute - travel_time_next) % 60
-                            )
-                            if disponibility.start_time < latest_end:
-                                available_slots.append({
-                                    "start": disponibility.start_time.strftime("%H:%M"),
-                                    "end": latest_end.strftime("%H:%M")
-                                })
-                                if (schedule_start_time >= disponibility.start_time and schedule_end_time <= latest_end):
-                                    if total_travel_time < min_travel_time:
-                                        min_travel_time = total_travel_time
-                                        best_position = i
-                    else:  # No existing schedules
-                        available_slots.append({
-                            "start": disponibility.start_time.strftime("%H:%M"),
-                            "end": disponibility.end_time.strftime("%H:%M")
-                        })
-                        best_position = 0
-                elif i == len(all_schedules):  # After last schedule
-                    last_schedule = all_schedules[-1]
-                    if last_schedule.schedule_end_time < disponibility.end_time:
-                        earliest_start = last_schedule.schedule_end_time.replace(
-                            hour=(last_schedule.schedule_end_time.hour * 60 + last_schedule.schedule_end_time.minute + travel_time_previous) // 60,
-                            minute=(last_schedule.schedule_end_time.hour * 60 + last_schedule.schedule_end_time.minute + travel_time_previous) % 60
-                        )
-                        if earliest_start < disponibility.end_time:
-                            available_slots.append({
-                                "start": earliest_start.strftime("%H:%M"),
-                                "end": disponibility.end_time.strftime("%H:%M")
-                            })
-                            if (schedule_start_time >= earliest_start and schedule_end_time <= disponibility.end_time):
-                                if total_travel_time < min_travel_time:
-                                    min_travel_time = total_travel_time
-                                    best_position = i
-                else:  # Between schedules
-                    current_schedule = all_schedules[i-1]
-                    next_schedule = all_schedules[i]
-                    
-                    earliest_start = current_schedule.schedule_end_time.replace(
-                        hour=(current_schedule.schedule_end_time.hour * 60 + current_schedule.schedule_end_time.minute + travel_time_previous) // 60,
-                        minute=(current_schedule.schedule_end_time.hour * 60 + current_schedule.schedule_end_time.minute + travel_time_previous) % 60
-                    )
-                    
-                    latest_end = next_schedule.schedule_start_time.replace(
-                        hour=(next_schedule.schedule_start_time.hour * 60 + next_schedule.schedule_start_time.minute - travel_time_next) // 60,
-                        minute=(next_schedule.schedule_start_time.hour * 60 + next_schedule.schedule_start_time.minute - travel_time_next) % 60
-                    )
-                    
-                    if earliest_start < latest_end:
-                        available_slots.append({
-                            "start": earliest_start.strftime("%H:%M"),
-                            "end": latest_end.strftime("%H:%M")
-                        })
-                        if (schedule_start_time >= earliest_start and schedule_end_time <= latest_end):
-                            if total_travel_time < min_travel_time:
-                                min_travel_time = total_travel_time
-                                best_position = i
-
-            if best_position is None:
-                if available_slots:
-                    raise serializers.ValidationError({
-                        "message": "Não há tempo suficiente para viajar entre os agendamentos.",
-                        "available_time": available_slots
-                    })
-                else:
-                    raise serializers.ValidationError({
-                        "message": "Não há horários disponíveis para este agendamento.",
-                        "available_time": []
-                    })
-
+            if position > 0:
+                origin = f"{all_schedules[position-1].address.latitude},{all_schedules[position-1].address.longitude}"
+                destination = f"{address.latitude},{address.longitude}"
+                route = gmaps.directions(origin, destination, mode="driving")
+                travel_time_previous = int(route[0]['legs'][0]['duration']['value'] / 60)
+                total_travel_time += travel_time_previous
+            
+            if position < len(all_schedules):
+                origin = f"{address.latitude},{address.longitude}"
+                destination = f"{all_schedules[position].address.latitude},{all_schedules[position].address.longitude}"
+                route = gmaps.directions(origin, destination, mode="driving")
+                travel_time_next = int(route[0]['legs'][0]['duration']['value'] / 60)
+                total_travel_time += travel_time_next
         except Exception as e:
-            if isinstance(e, serializers.ValidationError):
-                raise e
-            print("error: ", e)
+            print("error calculating travel times: ", e)
             raise serializers.ValidationError({
                 "message": "Erro ao calcular rotas. Por favor, tente novamente.",
                 "available_time": []
             })
+        
+        return travel_time_previous, travel_time_next, total_travel_time
 
-        schedule = super().create(validated_data)
-        return schedule
+    def find_available_slots(self, disponibility, all_schedules, schedule_start_time, schedule_end_time, address):
+        available_slots = []
+        best_position = None
+        min_travel_time = float('inf')
 
+        for i in range(len(all_schedules) + 1):
+            travel_time_previous, travel_time_next, total_travel_time = self.calculate_travel_times(address, all_schedules, i)
 
+            if i == 0:
+                if all_schedules:
+                    first_schedule = all_schedules[0]
+                    if first_schedule.schedule_start_time > disponibility.start_time:
+                        latest_end = first_schedule.schedule_start_time.replace(
+                            hour=(first_schedule.schedule_start_time.hour * 60 + first_schedule.schedule_start_time.minute - travel_time_next) // 60,
+                            minute=(first_schedule.schedule_start_time.hour * 60 + first_schedule.schedule_start_time.minute - travel_time_next) % 60
+                        )
+                        if disponibility.start_time < latest_end:
+                            available_slots.append({
+                                "start": disponibility.start_time.strftime("%H:%M"),
+                                "end": latest_end.strftime("%H:%M")
+                            })
+                            if (schedule_start_time >= disponibility.start_time and schedule_end_time <= latest_end):
+                                if total_travel_time < min_travel_time:
+                                    min_travel_time = total_travel_time
+                                    best_position = i
+                else:
+                    available_slots.append({
+                        "start": disponibility.start_time.strftime("%H:%M"),
+                        "end": disponibility.end_time.strftime("%H:%M")
+                    })
+                    best_position = 0
+            elif i == len(all_schedules):
+                last_schedule = all_schedules[-1]
+                if last_schedule.schedule_end_time < disponibility.end_time:
+                    earliest_start = last_schedule.schedule_end_time.replace(
+                        hour=(last_schedule.schedule_end_time.hour * 60 + last_schedule.schedule_end_time.minute + travel_time_previous) // 60,
+                        minute=(last_schedule.schedule_end_time.hour * 60 + last_schedule.schedule_end_time.minute + travel_time_previous) % 60
+                    )
+                    if earliest_start < disponibility.end_time:
+                        available_slots.append({
+                            "start": earliest_start.strftime("%H:%M"),
+                            "end": disponibility.end_time.strftime("%H:%M")
+                        })
+                        if (schedule_start_time >= earliest_start and schedule_end_time <= disponibility.end_time):
+                            if total_travel_time < min_travel_time:
+                                min_travel_time = total_travel_time
+                                best_position = i
+            else:
+                current_schedule = all_schedules[i-1]
+                next_schedule = all_schedules[i]
+                
+                earliest_start = current_schedule.schedule_end_time.replace(
+                    hour=(current_schedule.schedule_end_time.hour * 60 + current_schedule.schedule_end_time.minute + travel_time_previous) // 60,
+                    minute=(current_schedule.schedule_end_time.hour * 60 + current_schedule.schedule_end_time.minute + travel_time_previous) % 60
+                )
+                
+                latest_end = next_schedule.schedule_start_time.replace(
+                    hour=(next_schedule.schedule_start_time.hour * 60 + next_schedule.schedule_start_time.minute - travel_time_next) // 60,
+                    minute=(next_schedule.schedule_start_time.hour * 60 + next_schedule.schedule_start_time.minute - travel_time_next) % 60
+                )
+                
+                if earliest_start < latest_end:
+                    available_slots.append({
+                        "start": earliest_start.strftime("%H:%M"),
+                        "end": latest_end.strftime("%H:%M")
+                    })
+                    if (schedule_start_time >= earliest_start and schedule_end_time <= latest_end):
+                        if total_travel_time < min_travel_time:
+                            min_travel_time = total_travel_time
+                            best_position = i
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     requested_fields = self.context.get('request').query_params.get('fields')
-    #     if requested_fields:
-    #         requested_fields = requested_fields.split(',')
-    #         if 'str' not in requested_fields:
-    #             self.fields.pop('str', None)
-    #     else:
-    #         self.fields.pop('str', None)
+        if best_position is None:
+            if available_slots:
+                raise serializers.ValidationError({
+                    "message": "Não há tempo suficiente para viajar entre os agendamentos.",
+                    "available_time": available_slots
+                })
+            else:
+                raise serializers.ValidationError({
+                    "message": "Não há horários disponíveis para este agendamento.",
+                    "available_time": []
+                })
 
-    # def get_str(self, obj):
-    #     return str(obj)
-    
+        return best_position
+
+    def create(self, validated_data):
+        schedule_agent = validated_data.get("schedule_agent")
+        schedule_date = validated_data.get("schedule_date")
+        schedule_start_time = validated_data.get("schedule_start_time")
+        schedule_end_time = validated_data.get("schedule_end_time")
+        address = validated_data.get("address")
+
+        disponibility = self.validate_agent_availability(
+            schedule_agent, schedule_date, schedule_start_time, schedule_end_time
+        )
+
+        existing_schedules = self.check_schedule_conflicts(
+            schedule_agent, schedule_date, schedule_start_time, schedule_end_time, disponibility=disponibility
+        )
+
+        self.find_available_slots(
+            disponibility, list(existing_schedules), schedule_start_time, schedule_end_time, address
+        )
+
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        schedule_agent = validated_data.get("schedule_agent", instance.schedule_agent)
+        schedule_date = validated_data.get("schedule_date", instance.schedule_date)
+        schedule_start_time = validated_data.get("schedule_start_time", instance.schedule_start_time)
+        schedule_end_time = validated_data.get("schedule_end_time", instance.schedule_end_time)
+        address = validated_data.get("address", instance.address)
+
+        disponibility = self.validate_agent_availability(
+            schedule_agent, schedule_date, schedule_start_time, schedule_end_time
+        )
+
+        existing_schedules = self.check_schedule_conflicts(
+            schedule_agent, schedule_date, schedule_start_time, schedule_end_time, instance, disponibility=disponibility
+        )
+
+        self.find_available_slots(
+            disponibility, list(existing_schedules), schedule_start_time, schedule_end_time, address
+        )
+
+        return super().update(instance, validated_data)
+
 
 class AnswerSerializer(BaseSerializer):
     class Meta(BaseSerializer.Meta):
