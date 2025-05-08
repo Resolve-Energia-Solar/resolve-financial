86709aa48d0e3d0f00e4ad13935ab54e1d796f72
Thiago Menezes <thimenezes19@gmail.com>
2025-05-08 19:14:24 -0300

    add journey permission

--- resolve_crm/models.py
+++ resolve_crm/models.py
@@ -1270,6 +1270,7 @@ class Project(models.Model):
         ]
         permissions = [
             ('can_change_unready_project', 'Can change unready project'),
+            ('can_view_journey', 'Can view journey'),
         ]
     
     def __str__(self):
