67c8ad6bff827cc7e1c44909186d3f5297b45f0e
Thiago Menezes <thimenezes19@gmail.com>
2025-05-17 10:02:17 -0300

    comment webhook

--- resolve_crm/signals.py
+++ resolve_crm/signals.py
@@ -14,88 +14,88 @@ from django.db import transaction
 
 logger = logging.getLogger(__name__)
 
-def send_webhook_request(web_hook_url, data, secret):
-    headers = {
-        'Content-Type': 'application/json',
-        'X-Hook-Secret': secret
-    }
-    try:
-        response = requests.post(web_hook_url, json=data, headers=headers)
-        response.raise_for_status()
-        logger.info(f"Webhook enviado com sucesso: {response.status_code}")
-    except requests.exceptions.RequestException as e:
-        logger.error(f"Erro ao enviar o webhook: {e}")
+# def send_webhook_request(web_hook_url, data, secret):
+#     headers = {
+#         'Content-Type': 'application/json',
+#         'X-Hook-Secret': secret
+#     }
+#     try:
+#         response = requests.post(web_hook_url, json=data, headers=headers)
+#         response.raise_for_status()
+#         logger.info(f"Webhook enviado com sucesso: {response.status_code}")
+#     except requests.exceptions.RequestException as e:
+#         logger.error(f"Erro ao enviar o webhook: {e}")
 
 
-def get_model_data(instance):
-    data = {}
-    for field in instance._meta.get_fields():
-        field_name = field.name
+# def get_model_data(instance):
+#     data = {}
+#     for field in instance._meta.get_fields():
+#         field_name = field.name
 
-        try:
-            field_value = getattr(instance, field_name, None)
-        except Exception:
-            continue
+#         try:
+#             field_value = getattr(instance, field_name, None)
+#         except Exception:
+#             continue
 
-        if field.one_to_many or field.many_to_many:
-            field_value = [obj.id for obj in field_value.all()] if field_value else []
-        elif field.is_relation:
-            field_value = field_value.id if field_value else None
-        elif hasattr(field_value, 'url'):
-            field_value = field_value.url if field_value else None
-        elif hasattr(field_value, 'isoformat'):
-            field_value = field_value.isoformat()
+#         if field.one_to_many or field.many_to_many:
+#             field_value = [obj.id for obj in field_value.all()] if field_value else []
+#         elif field.is_relation:
+#             field_value = field_value.id if field_value else None
+#         elif hasattr(field_value, 'url'):
+#             field_value = field_value.url if field_value else None
+#         elif hasattr(field_value, 'isoformat'):
+#             field_value = field_value.isoformat()
 
-        data[field_name] = field_value
+#         data[field_name] = field_value
 
-    return data
-
-
-@receiver(post_save, sender=Project)
-def post_create_project(sender, instance, created, **kwargs):
-    if not instance.project_number:
-        generate_project_number.delay(instance.pk)
+#     return data
 
 
-@receiver(post_save)
-def send_webhook_on_save(sender, instance, created, **kwargs):
-    if 'migrate' in sys.argv or 'test' in sys.argv:
-        return
+# @receiver(post_save, sender=Project)
+# def post_create_project(sender, instance, created, **kwargs):
+#     if not instance.project_number:
+#         generate_project_number.delay(instance.pk)
 
-    try:
-        content_type = ContentType.objects.get_for_model(sender)
-    except ContentType.DoesNotExist:
-        return
 
-    event_type = 'C' if created else 'U'
-    webhooks = Webhook.objects.filter(
-        content_type=content_type,
-        event=event_type,
-        is_active=True
-    )
+# @receiver(post_save)
+# def send_webhook_on_save(sender, instance, created, **kwargs):
+#     if 'migrate' in sys.argv or 'test' in sys.argv:
+#         return
 
-    if webhooks.exists():
-        data = get_model_data(instance)
+#     try:
+#         content_type = ContentType.objects.get_for_model(sender)
+#     except ContentType.DoesNotExist:
+#         return
+
+#     event_type = 'C' if created else 'U'
+#     webhooks = Webhook.objects.filter(
+#         content_type=content_type,
+#         event=event_type,
+#         is_active=True
+#     )
+
+#     if webhooks.exists():
+#         data = get_model_data(instance)
         
-        for webhook in webhooks:
-            send_webhook_request(webhook.url, data, webhook.secret)
+#         for webhook in webhooks:
+#             send_webhook_request(webhook.url, data, webhook.secret)
 
 
-@receiver(post_delete)
-def send_webhook_on_delete(sender, instance, **kwargs):
-    content_type = ContentType.objects.get_for_model(sender)
+# @receiver(post_delete)
+# def send_webhook_on_delete(sender, instance, **kwargs):
+#     content_type = ContentType.objects.get_for_model(sender)
 
-    webhooks = Webhook.objects.filter(
-        content_type=content_type,
-        event='D',
-        is_active=True
-    )
+#     webhooks = Webhook.objects.filter(
+#         content_type=content_type,
+#         event='D',
+#         is_active=True
+#     )
 
-    if webhooks.exists():
-        data = get_model_data(instance)
+#     if webhooks.exists():
+#         data = get_model_data(instance)
         
-        for webhook in webhooks:
-            send_webhook_request(webhook.url, data, webhook.secret)
+#         for webhook in webhooks:
+#             send_webhook_request(webhook.url, data, webhook.secret)
 
 
 @receiver(post_save, sender=Attachment)
