df5872b33625360626b8676074588d825cc5c4ee
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-05-28 09:55:22 -0300

    fix: add StopConsumer to kill taks before timeout

--- field_services/consumers.py
+++ field_services/consumers.py
@@ -1,23 +1,25 @@
 import json
-from asgiref.sync import sync_to_async, async_to_sync
-from channels.layers import get_channel_layer
-from channels.generic.websocket import AsyncWebsocketConsumer
 import logging
+
+from channels.exceptions import StopConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
 
 
 logger = logging.getLogger(__name__)
 
+
 class LocationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user = self.scope['user']
-        self.group_name = None 
+        self.user = self.scope["user"]
+        self.group_name = None
 
         if not self.user.is_authenticated:
             await self.close()
             return
-        
+
         is_supervisor = await sync_to_async(
-           self.user.user_permissions.filter(codename="change_schedule").exists
+            self.user.user_permissions.filter(codename="change_schedule").exists
         )()
 
         is_client = await sync_to_async(
@@ -38,7 +40,9 @@ class LocationConsumer(AsyncWebsocketConsumer):
         if self.group_name:
             await self.channel_layer.group_add(self.group_name, self.channel_name)
             await self.accept()
-            logger.info(f"Usuário {self.user.username} conectado ao grupo {self.group_name}")
+            logger.info(
+                f"Usuário {self.user.username} conectado ao grupo {self.group_name}"
+            )
         else:
             await self.close()
 
@@ -54,22 +58,28 @@ class LocationConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps(data))
 
     async def receive(self, text_data):
-        try:   
+        try:
             data = json.loads(text_data)
         except json.JSONDecodeError:
             await self.send(text_data=json.dumps({"error": "Invalid data format"}))
             return
-        
-        #enviar para o cliente e para os supervisores
+
+        # enviar para o cliente e para os supervisores
         customer_id = data.get("custumer_id")
 
         if customer_id:
-            await self.channel_layer.group_send(f"client_{customer_id}", {
-                "type": "location_update",
-                "data": data
-            })
+            await self.channel_layer.group_send(
+                f"client_{customer_id}", {"type": "location_update", "data": data}
+            )
 
-        await self.channel_layer.group_send("supervisors", {    
-            "type": "location_update",
-            "data": data
-        })
+        await self.channel_layer.group_send(
+            "supervisors", {"type": "location_update", "data": data}
+        )
+
+    async def disconnect(self, close_code):
+        if self.group_name:
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
+            logger.info(f"Usuário desconectado do grupo {self.group_name}")
+        else:
+            logger.error("Erro: Nenhum grupo definido para este WebSocket")
+        raise StopConsumer()
