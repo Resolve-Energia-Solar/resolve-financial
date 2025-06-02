db339cbd07e8bbefbcbac32b30785cb7e12e9727
matheusmacias <matheusmacias3@gmail.com>
2025-06-01 21:10:58 -0300

    refactor: validate schadule last 24h only create

--- field_services/serializers.py
+++ field_services/serializers.py
@@ -45,25 +45,31 @@ class ScheduleSerializer(BaseSerializer):
         fields = '__all__'
 
     def validate(self, attrs):
-        schedule_date = attrs.get("schedule_date")
-        schedule_start_time = attrs.get("schedule_start_time")
+        if self.instance is not None:
+            return attrs
+        
+        service = attrs.get("service")
 
-        if schedule_date and schedule_start_time:
-            schedule_datetime = datetime.combine(schedule_date, schedule_start_time)
+        if service and service.name == "Serviço de Vistoria":
+            schedule_date = attrs.get("schedule_date")
+            schedule_start_time = attrs.get("schedule_start_time")
 
-            if is_naive(schedule_datetime):
-                schedule_datetime = make_aware(schedule_datetime)
+            if schedule_date and schedule_start_time:
+                schedule_datetime = datetime.combine(schedule_date, schedule_start_time)
 
-            now = timezone.now()
+                if is_naive(schedule_datetime):
+                    schedule_datetime = make_aware(schedule_datetime)
 
-            deadline_datetime = (schedule_datetime - timedelta(days=1)).replace(
-                hour=17, minute=0, second=0, microsecond=0
-            )
+                now = timezone.now()
 
-            if schedule_datetime - now < timedelta(hours=24) and now > deadline_datetime:
-                raise serializers.ValidationError(
-                    "Vistorias agendadas com menos de 24 horas de antecedência só são aceitas se forem registradas até às 17h do dia anterior."
+                deadline_datetime = (schedule_datetime - timedelta(days=1)).replace(
+                    hour=17, minute=0, second=0, microsecond=0
                 )
+
+                if schedule_datetime - now < timedelta(hours=24) and now > deadline_datetime:
+                    raise serializers.ValidationError(
+                        "Vistorias agendadas com menos de 24 horas de antecedência só são aceitas se forem registradas até às 17h do dia anterior."
+                    )
 
         return attrs
 
