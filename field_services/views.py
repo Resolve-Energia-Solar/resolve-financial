a789f80a4d80dbe1755eca7bab6b6c333e20918f
Thiago Menezes <thimenezes19@gmail.com>
2025-06-04 16:17:29 -0300

    fix: fix schedule

--- field_services/views.py
+++ field_services/views.py
@@ -219,7 +219,7 @@ class ScheduleViewSet(BaseModelViewSet):
 
     @method_decorator(cache_page(60 * 5))
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
+        queryset = self.filter_queryset(self.get_queryset())
 
         page = self.paginate_queryset(queryset)
         if page is not None:
