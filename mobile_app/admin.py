e462bf085914f453a08cd7069fa8e261c3254ec8
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-06-05 15:52:00 -0300

    style: indent mobile_app/admin.py

--- mobile_app/admin.py
+++ mobile_app/admin.py
@@ -4,29 +4,29 @@ from .models import API, Discount, Product, Reel, Media
 
 @admin.register(API)
 class APIAdmin(admin.ModelAdmin):
-    list_display = ('name', 'url')
-    search_fields = ('name',)
+    list_display = ("name", "url")
+    search_fields = ("name",)
 
 
 @admin.register(Discount)
 class DiscountAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'link')
-    search_fields = ('title',)
+    list_display = ("title", "description", "link")
+    search_fields = ("title",)
 
 
 @admin.register(Reel)
 class ReelAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'link')
-    search_fields = ('title',)
+    list_display = ("title", "description", "link")
+    search_fields = ("title",)
 
 
 @admin.register(Media)
 class MediaAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'link')
-    search_fields = ('title',)
+    list_display = ("title", "description", "link")
+    search_fields = ("title",)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price')
-    search_fields = ('name',)
+    list_display = ("name", "description", "price")
+    search_fields = ("name",)
