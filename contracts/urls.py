eb4060953c8a9cb7bd423b8b9f9ae0c3e1794b76
Leonardo Costa <leonardoabreu1212@gmail.com>
2025-06-07 15:14:24 -0300

    feat: add addendum pdf generation

--- contracts/urls.py
+++ contracts/urls.py
@@ -1,5 +1,15 @@
+from django.urls import path
+
 from api.urls import router
-from .views import SicoobRequestViewSet
+from .views import GenerateAddendumPDF, SicoobRequestViewSet
 
 
-router.register('sicoob-requests', SicoobRequestViewSet, basename='sicoob-request')
+router.register("sicoob-requests", SicoobRequestViewSet, basename="sicoob-request")
+
+urlpatterns = [
+    path(
+        "generate-addendum-pdf/",
+        GenerateAddendumPDF.as_view(),
+        name="generate-addendum-pdf",
+    ),
+]
